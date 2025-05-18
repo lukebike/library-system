1ec35bdd2348a4e253a12079c6d2fe3707bd3180
lukebike <lukesalem2002@gmail.com>
2025-05-18 13:50:47 +0200

    Functional BOOK and AUTHOR routes

---
+++ src/main/java/com/storedemo/librarysystem/Repositories/BookRepository.java
@@ -0,0 +1,17 @@
+package com.storedemo.librarysystem.Repositories;
+
+import com.storedemo.librarysystem.Entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+   Optional<Book> findByTitleIgnoreCase(String title);
+
+   List<Book> findByAuthorId(Long authorId);
+
+}
