0f14817d816bdf6c9df7b9c36dedd89dd03f77fa
Luke <lukesalem2002@gmail.com>
2025-09-11 10:59:47 +0200

    Added my frontend to the allowed origins

--- src/main/java/com/storedemo/librarysystem/Config/SecurityConfig.java
+++ src/main/java/com/storedemo/librarysystem/Config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://mybibliotek.netlify.app"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
